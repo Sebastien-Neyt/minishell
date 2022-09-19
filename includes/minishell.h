e46861ce414b8065b177606960fb653bbd02d9fd
Sebastien Neyt <sneyt@fu-r7-p4.s19.be>
2022-09-19 12:13:49 +0200

    kick-off

---
+++ includes/minishell.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sneyt <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/19 12:04:20 by sneyt             #+#    #+#             */
+/*   Updated: 2022/09/19 12:08:20 by sneyt            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+#define MINISHELL_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+
+int	ft_strcmp(char const *s1, char const *s2);
+
+#endif
