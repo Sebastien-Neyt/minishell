91e33453abeb9f7e38ddd10b9e5b6042b272a630
ccollards19 <cy.collard@gmail.com>
2022-09-26 10:24:38 +0200

    addded more colors, t_cmd struct and test_command fuction to .h

--- includes/minishell.h
+++ includes/minishell.h
@@ -20,13 +20,29 @@
 # include <readline/history.h>
 # include <sys/wait.h>
 
-# define RESET "\033[0m"
-# define GREEN "\033[32m"
+# define RESET	"\033[0m"
+# define BLACK	"\033[0;30m"
+# define RED	"\033[0;31m"
+# define GREEN	"\033[0;32m"
+# define YELLOW	"\033[0;33m"
+# define BLUE	"\033[0;34m"
+# define PURPLE	"\033[0;35m"
+# define CYAN	"\033[0;36m"
+# define WHITE	"\033[0;37m"
 # define BOLDGREEN "\033[1m\033[32m"
 
 typedef struct s_shell {
 	char	**envparams;
 } 	t_shell;
+
+typedef struct s_cmd {
+	char	*name;
+	char	**args;
+	char	**envp;
+} 	t_cmd;
+
+// test_command.c
+int	test_command(char *argv, char *envp);
 
 int	ft_strcmp(char const *s1, char const *s2);
 //////////////////////////////////////////////////
