a9e7256bae0b4f2fda2931fa680027f9345d8359
ccollards19 <cy.collard@gmail.com>
2022-11-04 14:13:44 +0100

    changed main

--- srcs/main.c
+++ srcs/main.c
@@ -41,3 +41,53 @@ int	main(int argc, char **argv, char **envp)
 	
 	return (0);
 }
+/*
+ * launch an ifinite loop that will:
+ *	readline
+ *	parse_line
+ *	tokenize_line
+ *	append_line while line not done (ex: pipe token at the end of line)
+ *	add to history
+ *	free line
+ *	repeat
+ *
+void	read_exec_loop(t_shell *minishell)
+{
+	while (1)
+	{
+		minishell->line = readline("minishell");
+		if (minishell.line == NULL)
+			ft_exit(NULL, minishell);
+		parse_line(&minishell);
+		tokenize_line(&minishell);
+		while (!minishell->line_done)
+			append_line(minishell);
+		execute_line(&minishell);
+		rl_add_history(minishell->line);
+		free(minishell->line);
+		minishell->line = NULL;
+	}
+}
+*
+*
+* declare struct
+* initialize struct
+* check arg nbr
+* initialize signals
+* call the main read exec loop
+* the program should never exit from the main
+*
+int	main(int argc, char *argv[], char *envp[])
+{
+	t_shell minishell;
+
+	init_shell_struct(&minishell);
+	if (argc > 1)
+		terminate(NULL, ERR_ARGNBR);
+	sig_init();
+	init_envs(&minishell, envp);
+	read_exec_loop(&minishell);
+	terminate(&minishell, NULL);
+	return (1);
+}
+*/
