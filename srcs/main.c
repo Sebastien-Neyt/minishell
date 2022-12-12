28f41f690705e3fc48bb06e5dccc0a6cbf66364d
ccollards19 <cy.collard@gmail.com>
2022-12-12 15:48:34 +0100

    get heredoc done

--- srcs/main.c
+++ srcs/main.c
@@ -12,6 +12,25 @@
 
 #include "../includes/minishell.h"
 
+int	append_str(char **str1, char *str2)
+{
+	char	*tmp;
+
+	if (str2 == NULL)
+		return 1;
+	if (*str1 == NULL)
+	{
+		*str1 = str2;
+		return 1;
+	}
+	tmp = ft_strjoin(*str1, str2);
+	if (tmp == NULL)
+		return 0;
+	free(*str1);
+	*str1 = tmp;
+	return 1;
+}
+
 void	parse_line(t_shell *minishell)
 {	
 	//printf("TEST//////////////////////%s\n", minishell->line);
@@ -20,12 +39,10 @@ void	parse_line(t_shell *minishell)
 	parse_list(minishell);
 	trim_pipeline(minishell);
 	tokenize_line(minishell);
-	print_pipeline(minishell);//TEST	
 }
 
 void	append_line(t_shell *minishell)
 {
-	char *tmp;
 
 	minishell->line_tmp = minishell->line;
 	minishell->line = readline(">");
@@ -33,9 +50,41 @@ void	append_line(t_shell *minishell)
 		ft_exit(minishell, UNXPCTD_EOF);
 	parse_line(minishell);
 	//check_syntax_error(minishell);
-	tmp = minishell->line;
-	minishell->line = ft_strjoin(minishell->line_tmp, minishell->line);
-	free(tmp);
+	if (append_str(&(minishell->line_tmp), minishell->line) == 0)
+		ft_exit(minishell, FAILED_MALLOC);
+	free(minishell->line);
+	minishell->line = minishell->line_tmp;
+}
+
+void	get_heredoc(t_shell *minishell, t_list *pipeline, char *input, int control)
+{
+	pipeline = minishell->pipeline;
+	minishell->line_tmp = minishell->line;
+	while (pipeline && pipeline->token != HEREDOC_DEL)
+		pipeline = pipeline->next;
+	while (control)
+	{
+		minishell->line = readline(">");
+		if (minishell->line == NULL)
+			break;
+		if (append_str(&(minishell->line_tmp), minishell->line) == 0)
+			control = 0;
+		if (ft_strcmp(minishell->line, pipeline->word) == 0)
+			break;
+		if (append_str(&(input), minishell->line) == 0)
+			control = 0;
+	}
+	free(pipeline->word);
+	pipeline->word = input;
+	pipeline->token = HEREDOC_CONTENT;
+	if (minishell->line == NULL)
+		perror("warning: here-document delimited by end-of-file\n");
+	else
+		free(minishell->line);
+	minishell->line = minishell->line_tmp;
+	minishell->line_tmp = NULL;
+	if (control == 0)
+		ft_exit(minishell, FAILED_MALLOC);
 }
 
 int	line_not_done(t_shell *minishell)
@@ -46,11 +95,11 @@ int	line_not_done(t_shell *minishell)
 	while (pipeline && pipeline->next)
 	{
 		if (pipeline->token == HEREDOC_DEL)
-			return (1);
+			return (HEREDOC_DEL);
 		pipeline = pipeline->next;
 	}
-	if (pipeline->token == PIPE)
-		return (1);
+	if (pipeline->token == PIPE || pipeline->token == HEREDOC_DEL)
+		return (pipeline->token);
 	return (0);
 }
 
@@ -66,6 +115,9 @@ int	line_not_done(t_shell *minishell)
  */
 void	read_exec_loop(t_shell *minishell)
 {
+	int not_done;
+
+	not_done = 0;
 	while (1)
 	{
 		minishell->line = readline("minishell"RED"<3 "WHITE"");
@@ -73,11 +125,20 @@ void	read_exec_loop(t_shell *minishell)
 			ft_exit(minishell, DEFAULT_MSG);
 		parse_line(minishell);
 		//check_syntax_error();
-		while (line_not_done(minishell))
-			append_line(minishell);
+		not_done = line_not_done(minishell);
+		while (not_done)
+		{
+			if (not_done == HEREDOC_DEL)
+				get_heredoc(minishell, NULL, NULL, 1);
+			else if (not_done == PIPE)
+				append_line(minishell);
+			not_done = line_not_done(minishell);
+		}
+		print_pipeline(minishell);//TEST	
 		execute_line(minishell);
 		//rl_add_history(minishell->line);
 		reset_line(minishell);
+		printf(GREEN"HEEEERE\n");
 	}
 }
 
